91741e29caf9fc9564ff3433119b15a0c69f55c3
Fernando Díaz <feeldiac@gmail.com>
2023-10-21 22:41:09 -0500

    feat: create product cards and shopping page components

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,60 +1,63 @@
 import {
-    NavLink,
-    Navigate,
-    Outlet,
-    RouterProvider,
-    createBrowserRouter,
+  NavLink,
+  Navigate,
+  Outlet,
+  RouterProvider,
+  createBrowserRouter,
 } from "react-router-dom";
-import logo from '../logo.svg';
-import { routes } from './routes';
+import logo from "../logo.svg";
+import { routes } from "./routes";
 import { Suspense } from "react";
 function Root() {
-
-    // eslint-disable-next-line no-lone-blocks
-    {/*
+  // eslint-disable-next-line no-lone-blocks
+  {
+    /*
         Hacer carga diferida de un componente o un módulo
-    */}
+    */
+  }
 
-    return (
-
-        <div className="main-layout">
-            <nav>
-                <img src={logo} alt="React logo" />
-                <ul>
-                    {routes.map(({ to, name }) => (
-                        <li key={to}>
-                            <NavLink
-                                to={to} className={({ isActive }) => isActive ? "nav-active" : ""}
-                            >
-                                {name}
-                            </NavLink>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-            <div id="detail">
-                <Outlet />
-            </div>
-        </div>
-    )
+  return (
+    <div className="main-layout">
+      <nav>
+        <img src={logo} alt="React logo" />
+        <ul>
+          {routes.map(({ to, name }) => (
+            <li key={to}>
+              <NavLink
+                to={to}
+                className={({ isActive }) => (isActive ? "nav-active" : "")}
+              >
+                {name}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <div id="detail">
+        <Outlet />
+      </div>
+    </div>
+  );
 }
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Root />,
-        children: routes.map(({ path, Component }) => ({ path, element: <Component /> }))
-    },
-    {
-        path: "/*",
-        element: <Navigate to={routes[0].to} replace />
-    }
+  {
+    path: "/",
+    element: <Root />,
+    children: routes.map(({ path, Component }) => ({
+      path,
+      element: <Component />,
+    })),
+  },
+  {
+    path: "/*",
+    element: <Navigate to={routes[0].to} replace />,
+  },
 ]);
 
-
 export const Navigation = () => {
-    return (
-        <Suspense fallback={<span>Loading...</span>}>
-            <RouterProvider router={router} />
-        </Suspense>
-    )
-}
+  return (
+    <Suspense fallback={<span>Loading...</span>}>
+      <RouterProvider router={router} />
+    </Suspense>
+  );
+};
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,35 +1,36 @@
-import { lazy, LazyExoticComponent } from 'react';
+import { lazy, LazyExoticComponent } from "react";
+import { ShoppingPage } from "../02-component-patterns/pages/ShoppingPage";
 
 type JSXComponent = () => JSX.Element;
 
 interface Route {
-    to: string;
-    path: string;
-    Component: LazyExoticComponent<JSXComponent> | JSXComponent; //¿Por qué tener la c capitalizada?
-    name: string;
+  to: string;
+  path: string;
+  Component: LazyExoticComponent<JSXComponent> | JSXComponent; //¿Por qué tener la c capitalizada?
+  name: string;
 }
 
-const Lazy1 = lazy(() => import('../01-lazyload/pages/LazyPage1'));
-const Lazy2 = lazy(() => import('../01-lazyload/pages/LazyPage2'));
-const Lazy3 = lazy(() => import('../01-lazyload/pages/LazyPage3'));
+const Lazy1 = lazy(() => import("../01-lazyload/pages/LazyPage1"));
+const Lazy2 = lazy(() => import("../01-lazyload/pages/LazyPage2"));
+const Lazy3 = lazy(() => import("../01-lazyload/pages/LazyPage3"));
 
 export const routes: Route[] = [
-    {
-        to: '/lazy1',
-        path: 'lazy1',
-        Component: Lazy1,
-        name: 'Lazy 1',
-    },
-    {
-        to: '/lazy2',
-        path: 'lazy2',
-        Component: Lazy2,
-        name: 'Lazy 2',
-    },
-    {
-        to: '/lazy3',
-        path: 'lazy3',
-        Component: Lazy3,
-        name: 'Lazy 3',
-    },
-]
+  {
+    to: "/",
+    path: "/",
+    Component: ShoppingPage,
+    name: "Shopping",
+  },
+  {
+    to: "/about",
+    path: "about",
+    Component: Lazy2,
+    name: "About",
+  },
+  {
+    to: "/users",
+    path: "users",
+    Component: Lazy3,
+    name: "Users",
+  },
+];
